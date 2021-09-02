8b024a9fb14d0d07e61a66fba0566adfebdaff7b
Noitcereon <thomaswowundercover@gmail.com>
2021-09-02 14:00:40 +0200

    comment + centered main

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -8,7 +8,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
         integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ asset('resources/css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}"> <!-- public/css/app.css -->
 </head>
 
 <body>
@@ -20,7 +20,7 @@
             </ul>
         </nav>
     </header>
-    <main>
+    <main class="container">
         @yield('content')
     </main>
 </body>
