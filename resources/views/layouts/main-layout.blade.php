3ecb5c579e238cbc6adf41c3f62c3f8bc200e785
Noitcereon <thomaswowundercover@gmail.com>
2021-09-03 13:58:55 +0200

    layout changes

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>B2B Annonce Platform</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}"> <!-- public/css/app.css -->
 </head>
 <body>
     <header>
+        
         <nav class="p-2">
             <ul class="nav justify-content-center">
                 <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
