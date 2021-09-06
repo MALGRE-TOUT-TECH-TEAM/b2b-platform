f0b2cb27b892a2847844396950018a769aaf620d
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2021-09-06 13:28:17 +0200

    user changes on model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,17 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname', 100);
-            $table->string('surname', 100);
-            $table->string('email', 191)->unique()->update();
-            $table->string('photo', 50)->update();
+            $table->string('firstname', 100)->update();
+            $table->string('surname', 100)->update();
+            $table->string('email', 191)->unique();
+            $table->string('photo', 50);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 191);
             $table->date('birthdate');
             $table->rememberToken();
             $table->string('telephone', 20);
             $table->string('gender', 20);
-            $table->tinyInteger('is_active');
+            $table->tinyInteger('is_active')->default(1)->update();
             $table->timestamps();
             $table->softDeletes('deleted_at');
         });
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get("dashboard", function () {
 Route::get('testing', function () {
     return view('testing');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
