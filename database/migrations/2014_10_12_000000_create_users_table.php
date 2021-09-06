9906146f22d5ffbbbcd08597756ad3e6e2fd460b
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2021-09-06 11:12:45 +0200

    Update User table
    Updated user table with more rows

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('firstname', 100);
+            $table->string('surname', 100);
+            $table->string('email', 191)->unique()->update();
+            $table->string('photo', 50)->update();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 191);
+            $table->date('birthdate');
             $table->rememberToken();
+            $table->string('telephone', 20);
+            $table->string('gender', 20);
+            $table->tinyInteger('is_active');
             $table->timestamps();
+            $table->softDeletes('deleted_at');
         });
     }
 
