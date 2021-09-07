18bd1e7f0167527d54f92cd230484623647f6a8e
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2021-09-07 13:47:57 +0200

    Login works + design
    redirecting to /login prompts the user to login. afterwards it redirects to the /dashboard page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,67 @@
 @extends('layouts.login')
 
 @section('content')
-<div class="container login-container red">
+<div class="login-container">
     <div class="login-box ">
-        <div class="login-register">
-            <div class="col-md-12">
-                <div class="row">
-                    <div class="col-md-3 red "><a href="#login"><h5>LOG IND</h5></a></div>
-                    <div class="col-md-9 red"><a href="#opret"><h5>OPRET KONTO</h5></a></div>
-                </div>
-                <div class="row red login-form">
-                    hej
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="login-register">
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-3 login-col "><a href="#login">LOG IND</a></div>
+                        <div class="col-md-9 opret-col"><a href="#opret">OPRET KONTO</a></div>
+                    </div>
+                    <div class="row login-form">
+                        <div class="row email-input-box">
+                            <label for="email">{{ __('E-MAIL') }}</label>
+                            <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        </div>
+                        <div class="row kode-input-box">
+                            <label for="password">{{ __('KODE') }}</label>
+                            <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+
+                        <div class="row accept-box"> 
+                            <div class="col-md-1 round column-checkbox">
+
+                            </div>
+                            <div class="col-md-6 column-text">
+                                <div class="col-md-1 round column-checkbox">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="checkbox" {{ old('remember') ? 'checked' : '' }}>
+                                    <label for="checkbox"></label>
+                                </div>
+                                <label for="checkbox">{{ __('Husk mig') }}</label>
+                            </div>
+                            <div class="col-md-5 column-text">
+                                @if (Route::has('password.request'))
+                                <label for="">
+                                    <a href="{{ route('password.request') }}">
+                                        {{ __('Glemt kodeord?') }}
+                                    </a>
+                                </label>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <button submit="type" class="login-btn">
+                                {{ __('Log Ind') }}                   
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
 </div>
 @endsection
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -18,7 +18,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
