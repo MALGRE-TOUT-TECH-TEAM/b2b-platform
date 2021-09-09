53c2082bd43fd90e10a1ef82c2b6101e57d788e3
Noitcereon <thomaswowundercover@gmail.com>
2021-09-09 13:49:33 +0200

    composer.lock update (added google api client)

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.main-layout')
 
 @section('content')
+<section class="row bg-primary" id="visitor-analytics">
+    <h1>Dashboard</h1>
+</section>
 <section class="row">
-    <h1>Here be data *woosh*</h1>
-    <div class="col-md-4 bg-primary">Column 1</div>
-    <div class="col-md-4 bg-secondary">Column 2</div>
-    <div class="col-md-4 bg-dark text-light">Column 3</div>
+    <h1>Winthers Rideudstyr</h1>
+    <div class="col-md-3 bg-primary">Column 1</div>
+    <div class="col-md-3 bg-secondary">Column 2</div>
+    <div class="col-md-3 bg-dark text-light">Column 3</div>
+    <div class="col-md-3 bg-danger text-light">Column 4</div>
 </section>
 @endsection
