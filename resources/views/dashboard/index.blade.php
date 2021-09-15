75ba3827f2a47563b5a953b0caf0b9f07adb3041
Noitcereon <thomaswowundercover@gmail.com>
2021-09-15 13:24:38 +0200

    Added spatie/laravel-analytics & removed google/apiclient

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,27 +1,30 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <section class="row bg-pink justify-content-around" id="visitor-analytics">
-        <div class="col-md-12">
-            <h1 class="text-light">Dashboard</h1>
+<section class="row bg-pink justify-content-around" id="visitor-analytics">
+    <div class="col-md-12">
+        <h1 class="text-light">Dashboard</h1>
+        <div class="row justify-content-around align-items-start pb-2">
+            <div class="col-lg-3 bg-light rounded">
+                <h3>Besøgende sidste syv dage</h3>
+            </div>
+            <div class="col-lg-3 bg-light rounded">
+                <h3>Besøgende lige nu</h3>
+            </div>
+            <div class="col-lg-5 bg-light rounded">
+                <h3>Besøgende sidste måned</h3>
+                Lang graf
+            </div>
         </div>
+    </div>
 
-        <div class="col-lg-3 bg-light rounded">
-            <h3>Besøgende sidste syv dage</h3>
-        </div>
-        <div class="col-lg-3 bg-light rounded">
-            <h3>Besøgende lige nu</h3>
-        </div>
-        <div class="col-lg-5 bg-light rounded">
-            <h3>Besøgende sidste måned</h3>
-            Lang graf
-        </div>
-    </section>
-    <section class="row pt-4">
-        <h2>Winthers Rideudstyr</h1>
-            <div class="col-md-3 rounded box-shadow">Aktive annoncer</div>
-            <div class="col-md-3 rounded">Visninger i alt</div>
-            <div class="col-md-3 rounded">Klik i alt</div>
-            <div class="col-md-3 rounded bg-dark">Administrer dine annoncer</div>
-    </section>
+
+</section>
+<section class="row pt-4">
+    <h2>Winthers Rideudstyr</h1>
+        <div class="col-md-3 rounded box-shadow">Aktive annoncer</div>
+        <div class="col-md-3 rounded">Visninger i alt</div>
+        <div class="col-md-3 rounded">Klik i alt</div>
+        <div class="col-md-3 rounded bg-dark">Administrer dine annoncer</div>
+</section>
 @endsection
