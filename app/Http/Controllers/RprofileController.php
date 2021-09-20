49fa2cdd74f5424a95ba31260962efd75639c6a3
Anders Hoejer <skinkens_email@yahoo.dk>
2021-09-20 13:25:20 +0200

    Hey

--- app/Http/Controllers/RprofileController.php
+++ app/Http/Controllers/RprofileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateUser;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -11,28 +12,25 @@ use Illuminate\Support\Facades\Redirect;
 class RprofileController extends Controller
 {
     // GET "frontpage" of this controller.
-    public function index(){
-        if (auth()->user()) 
-        { 
+    public function index()
+    {
+        if (auth()->user()) {
             return redirect(route('home'));
-        }
-        else
-        {
-            return view('register.rprofile'); 
+        } else {
+            return view('register.rprofile');
         }
     }
-    public function index2(){
-        if (auth()->user()) 
-        { 
+    public function index2()
+    {
+        if (auth()->user()) {
             return redirect(route('home'));
-        }
-        else
-        {
-            return view('register.opretprofile');        
+        } else {
+            return view('register.opretprofile');
         }
     }
 
-    function firstpt(Request $request){
+    function firstpt(Request $request)
+    {
         $data = $request->input();
         $request->session()->put('email', $data['email']);
         $request->session()->put('password', $data['password']);
@@ -40,24 +38,23 @@ class RprofileController extends Controller
         return redirect('/rprofile');
     }
 
-    protected function create(Request $data)
+    protected function create(CreateUser $data)
     {
-        $user = User::create([
-             'firstname' => $data['firstname'],
-            'surname' => $data['surname'],
-            'email'=> $data['email'],
-            'password' => Hash::make($data['password']),
-            'birthdate' => $data['birthdate'],
-            'gender' => $data['gender'],
-            'photo' => $data['photo'],
-            'telephone' => $data['telephone'],
-        ]);
-        Auth::login($user);
-        return redirect('/rcategory');
+        // $user = User::create([
+        //     'firstname' => $data['firstname'],
+        //     'surname' => $data['surname'],
+        //     'email' => $data['email'],
+        //     'password' => Hash::make($data['password']),
+        //     'birthdate' => $data['birthdate'],
+        //     'gender' => $data['gender'],
+        //     'photo' => $data['photo'],
+        //     'telephone' => $data['telephone'],
+        // ]);
+
+        $validatedUser = $data->validated();
+        $user = User::create($validatedUser);
+        if (Auth::attempt($user)) {
+            return redirect('/rcategory');
+        }
     }
-
-    
-    
-    
-
 }
