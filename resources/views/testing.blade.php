59ea57814edfdabb8089a44f80bc792f4748ec5a
Noitcereon <thomaswowundercover@gmail.com>
2021-09-22 14:48:20 +0200

    Messing with arrays

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -2,18 +2,45 @@
 
 @section('content')
 <h1>This is the Testing Page</h1>
-@if(isset($analyticsData))
-    <h2>Google Analytics Data</h2>
-    {{-- @foreach($variable as $key => $date)
+<h2>Array Testing</h2>
+@isset($myArray)
+<ul>
+    <li>{{ $myArray["First"] }}</li>
+    <li>{{ $myArray["Second"] }}</li>
+    <li>{{ $myArray["Third"] }}</li>
+    {{-- @php
+        foreach($myArray as $keyPair){
+           echo('<li>' $keyPair '</li>'); 
+        }
+    @endphp --}}
+</ul>
+@endisset
+
+@isset($simpleArray)
+    Simple Array
+    @foreach($item as $key => $value)
         
+    @endforeach
+@endisset
+
+@if(isset($analyticsData))
+<h2>Google Analytics Data</h2>
+<table class="table table stripped">
+    <tr>
+        <th>Date</th>
+        <th>Page Title</th>
+        <th>Visitors</th>
+        <th>Page Views</th>
+    </tr>
+    {{-- @foreach($item in $analyticsData)
+    <tr>
+        <td>{{ $item['date']}}</td>
+    <td>{{ $item['pageTitle'] }}</td>
+    <td>{{ $item['visitors'] }}</td>
+    <td>{{ $item['pageViews'] }}</td>
+    </tr>
     @endforeach --}}
+</table>
 
-    {{-- return [
-        'date' => Carbon::createFromFormat('Ymd', $dateRow[0]),
-        'pageTitle' => $dateRow[1],
-        'visitors' => (int) $dateRow[2],
-        'pageViews' => (int) $dateRow[3],
-    ]; --}}
 @endif
-
 @endsection
