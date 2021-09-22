6111131023f882aeef5f9b6f8b0cb709476506a0
Benjo6 <benjo3@godmail.dk>
2021-09-22 09:44:13 +0200

    Yeah, I'm surprised too.

--- routes/web.php
+++ routes/web.php
@@ -22,18 +22,17 @@ Route::get('testing', function () {
 
 
 
-Route::get("login", [App\Http\Controllers\LoginController::class, 'index']);
+
 Route::get("opretprofile", [App\Http\Controllers\RprofileController::class, 'index2']);
 Route::get("rprofile", [App\Http\Controllers\RprofileController::class, 'index']);
-Route::get("rcategory", [App\Http\Controllers\U2CController::class, 'get']);
-Route::get("raudience", [App\Http\Controllers\U2AController::class, 'get']);
+Route::get("rcategory", [App\Http\Controllers\U2CController::class, 'index']);
+Route::get("raudience", [App\Http\Controllers\U2AController::class, 'index']);
 
 
 
 
 
 
-Route::get("logmein", [App\Http\Controllers\LoginController::class, 'clogin']);
 Route::post("logout", [App\Http\Controllers\LoginController::class, 'logout']);
 Route::post("rcreate", [App\Http\Controllers\RprofileController::class, 'create']);
 Route::post("ccreate", [App\Http\Controllers\U2CController::class, 'create']);
