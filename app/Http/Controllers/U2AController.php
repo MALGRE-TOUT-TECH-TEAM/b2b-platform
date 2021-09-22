ea173c41e6bb8830b27572cd94625c1c2fb1db79
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2021-09-22 14:58:47 +0200

    UI til oprettelse af bruger
    2 get lavet om til index, + ui af bruger oprettelse er blevet lavet færdigt.

--- app/Http/Controllers/U2AController.php
+++ app/Http/Controllers/U2AController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 
 class U2AController extends Controller
 {
-    public function get()
+    public function index()
     {
         $audiences = Audience::all();
         return view('register.audience', compact('audiences'));
--- resources/views/register/audience.blade.php
+++ resources/views/register/audience.blade.php
@@ -1,3 +1,66 @@
+@extends('layouts.login')
+@section('assets')
+    <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+@section('content')
+
+<div class="profile-wrapper">
+    <div class="profile-box">
+        <form method="POST" action="acreate">
+            @csrf
+            <div class="category-info-grid">
+                <div>
+                    <div>
+                        <h5 class="cat-header-text">VÆLG MÅLGRUPPE</h5>
+                    </div>
+                    <div class="cat-inner">
+                        <div>
+                            <i class="cat-icon fa-solid fa-circle-info"></i>
+                        </div>
+                        <div class="info-title">
+                            <h6>Målgruppe</h6>
+                        </div>
+                        <div></div>
+                        <div class="info-title">
+                            <p>Vælg den eller de målgrupper, som dine produkter henvender sig til.</p>
+                        </div>
+                    </div>
+                </div>
+                <div></div>
+                <div class="progress-box">
+                    <img src="../media-assets/progress75.png" height="105" alt="">
+                </div>
+            </div>
+            <div class="categories-grid">
+
+                    @foreach($audiences as $data)
+                    <div class="category-button">
+                        <input class="hidden" type="checkbox" name="audiences[]" id="{{$data->id}}" value="{{$data->id}}">
+                        <label class="category-label" for="{{$data->id}}">{{$data->dan_name}}</label>
+                    </div>
+                    @endforeach            
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <button submit="type" class="profile-btn">
+                        {{ __('Næste') }}
+                    </button>
+                </div>
+                <div class="col-md-6">
+                    
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+
+
+
+
+
 <div>
     <h5>PAPA MIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h5>
     <form method="POST" action="acreate">
