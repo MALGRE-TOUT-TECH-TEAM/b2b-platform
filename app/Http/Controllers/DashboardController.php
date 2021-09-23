b533d771bc3f32c1829d14530d8c75d2864db67d
Noitcereon <thomaswowundercover@gmail.com>
2021-09-23 14:15:36 +0200

    Succeeded in showing the google data in a table

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,22 +15,22 @@ class DashboardController extends Controller
     }
     public function googleAnalytics()
     {
-        $analyticsData = Analytics::fetchVisitorsAndPageViews(Period::days(7));
-        $myArray = [
+        $simpleArray = [1, 2, 3, 4];
+        $associativeArray = [
             'First' => '1st',
             'Second' => '2nd',
             'Third' => '3rd'
         ];
-        $simpleArray = [1, 2, 3, 4];
-        $nestedArray = [
+        $multidimensionalArray = [
             'dates' => ['1st of January', '2nd of January', '3rd of January', '4th of January', '5th of January', '6th of January', '7th of January'],
             'visitors' => [2523, 5000, 3655, 3214, 2443, 3461, 1577],
             'page' => ['/dashboard', '/login', '/almightyAbovesHouse', '/over9000', '/suspiciousMammal', '/analytics', '/krom']
         ];
+        $analyticsData = Analytics::fetchVisitorsAndPageViews(Period::days(7));
         return view('testing')
-            ->with('analyticsData', $analyticsData)
-            ->with('myArray', $myArray)
             ->with('simpleArray', $simpleArray)
-            ->with('nestedArray', $nestedArray);
+            ->with('assArray', $associativeArray)
+            ->with('multidimensionalArray', $multidimensionalArray)
+            ->with('analyticsData', $analyticsData);
     }
 }
--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -6,70 +6,80 @@
 
 <!-- Simple array-->
 @isset($simpleArray)
-<h3>Simple Array</h3>
-@foreach($simpleArray as $number)
-@if ($loop->last)
-{{-- This is the last iteration --}}
-{{ $number }}
-@break
-@endif
-{{ $number }},
-@endforeach
+<div>
+    <h3>Simple Array</h3>
+    @foreach($simpleArray as $number)
+    @if ($loop->last)
+    {{-- This is the last iteration --}}
+    {{ $number }}
+    @break
+    @endif
+    {{ $number }},
+    @endforeach
+</div>
 @endisset
 
-<h3>Key Array</h3>
-@isset($myArray)
+<h3>Associative Array</h3>
+@isset($assArray)
 <ul>
-    <li>{{ $myArray["First"] }}</li>
-    <li>{{ $myArray["Second"] }}</li>
-    <li>{{ $myArray["Third"] }}</li>
-    @foreach($myArray as $key => $value)
+    <li>{{ $assArray["First"] }}</li>
+    <li>{{ $assArray["Second"] }}</li>
+    <li>{{ $assArray["Third"] }}</li>
+    @foreach($assArray as $key => $value)
     {{ $key }} = {{ $value }}<br>
     @endforeach
 </ul>
 @endisset
 
-<h3>"Nested" Arrays</h3>
-<ul>
-    @foreach($nestedArray as $key => $value)
-    <pre>
-         @php
-             print_r($key);
-         @endphp
-    </pre>
-    @endforeach
-</ul>
-
-{{-- $nestedArray = [
+@isset($multidimensionalArray)
+{{-- 
+$nestedArray = [
     'dates' => ['1st of January', '2nd of January', '3rd of January', '4th of January', '5th of January', '6th of January', '7th of January'],
     'visitors' => [2523, 5000, 3655, 3214, 2443, 3461, 1577],
     'page' => ['/dashboard', '/login', '/almightyAbovesHouse', '/over9000', '/suspiciousMammal', '/analytics', '/krom']
 ]; --}}
+<div>
+    <h3>"Nested" Arrays</h3>
+    <table class="table">
+        <tr>
+            <th>Date</th>
+            <th>Visitors</th>
+            <th>Page</th>
+        </tr>
+
+        @for($i = 0; $i < count($multidimensionalArray['dates']); $i++) 
+            <tr>
+                <td>{{ $multidimensionalArray['dates'][$i] }}</td>
+                <td>{{ $multidimensionalArray['visitors'][$i] }}</td>
+                <td>{{ $multidimensionalArray['page'][$i] }}</td>
+            </tr>
+        @endfor
+    </table>
+</div>
+@endisset
+
 
 
 @if(isset($analyticsData))
-<h2>Google Analytics Data</h2>
-<table class="table table stripped">
-    <tr>
-        <th>Date</th>
-        <th>Page Title</th>
-        <th>Visitors</th>
-        <th>Page Views</th>
-    </tr>
-    <pre>
-    @php
-        print_r($analyticsData)
-    @endphp
-    </pre>
-    {{-- @foreach($analyticsData as $key => $value)
-    <tr>
-        <td>{{ $key['date']}}</td>
-    <td>{{ $item['pageTitle'] }}</td>
-    <td>{{ $item['visitors'] }}</td>
-    <td>{{ $item['pageViews'] }}</td>
-    </tr>
-    @endforeach --}}
-</table>
+<div>
+    <h2>Google Analytics Data</h2>
+    <table class="table table stripped">
+        <tr>
+            <th>Date</th>
+            <th>Page Title</th>
+            <th>Visitors</th>
+            <th>Page Views</th>
+        </tr>
+        @foreach($analyticsData as $data)
+            <tr>
+                <td>{{ $data['date']}}</td>
+                <td>{{ $data['pageTitle'] }}</td>
+                <td>{{ $data['visitors'] }}</td>
+                <td>{{ $data['pageViews'] }}</td>
+            </tr>
+        @endforeach
+    </table>
 
+</div>
 @endif
 @endsection
