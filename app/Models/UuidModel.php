27b86148371129836c573469d955ffa03f29bc8b
Anders Hoejer <skinkens_email@yahoo.dk>
2021-09-24 14:44:23 +0200

    fixed user

--- app/Models/UuidModel.php
+++ app/Models/UuidModel.php
@@ -3,10 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Support\Str;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Database\Eloquent\Model;
 
-class UuidModel extends Model
+class UuidModel extends Model implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
 {
+
+    use Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail;
+
     protected static function boot()
     {
         parent::boot();
